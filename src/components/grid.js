98022c49b697db68d25184579b74cc9c7b8e57be
Alessandro Bartoli <alessandro@bartolilab.it>
2022-04-26 13:01:49 +0200

    Info comments in "index.js" and "grid.js".

--- src/components/grid.js
+++ src/components/grid.js
@@ -2,12 +2,17 @@ import * as React from 'react';
 
 const Grid = (props) => {
     return (
-    <>
-    <div className='grid grid-cols-1 md:grid-cols-4 xl:grid-cols-12 gap-6'>
-        {props.children}
-    </div>
-    </>
-)
+
+        <>
+            {/* 
+                In quetso caso i React fragments sono superflui, puoi toglierli.
+                Puoi togliere anche il return e mettere le parentesi tonde al componente.
+            */}
+            <div className='grid grid-cols-1 md:grid-cols-4 xl:grid-cols-12 gap-6'>
+                {props.children}
+            </div>
+        </>
+    );
 };
 
 export default Grid;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,16 @@ import Paragraph from '../components/paragraph';
 import Layout from '../components/layout';
 
 const IndexPage = () => (
-    <>
+  <>
     <Layout>
-        <Paragraph
-            fontSize="80px"
-            color='#258900'
-        >
-            Some text
-        </Paragraph>
+      {/* Anche qui, se wrappi tutto in Layout puoi togliere i React fragments. */}
+      <Paragraph
+        fontSize="80px"
+        color='#258900'
+      >
+        Some text
+      </Paragraph>
     </Layout>
-    </>
+  </>
 );
 export default IndexPage;
